18f308520e88ec44cb18be1da83e3ce1babd6cb6
vikas trivedi <vikastrivedi4522@gmail.com>
2024-12-19 19:40:03 +0530

    minor adjustments in nav icon script

--- navIcon.js
+++ navIcon.js
@@ -1,7 +1,14 @@
-const navIconBtn = document.getElementById("nav-icon-btn");
-const navInstructions = document.getElementById("nav-instruction");
+const navIconBtn = document.getElementById("nav-icon-btn");//Navbar icon
 
+const navInstructions = document.getElementById("nav-instruction");//Instructions paragraph
+
+//On nav icon clicked fire an event
 navIconBtn.addEventListener("click", () => {
+
+    // Get that nav is expanded or not
     const isNavVisible = navIconBtn.getAttribute("aria-expanded") === "true";
+    
+    // If the navbar is visible hide the instructions regarding to toggle navbar icon 
     navInstructions.style.display = isNavVisible ? "none" : "block";
+    
 });
